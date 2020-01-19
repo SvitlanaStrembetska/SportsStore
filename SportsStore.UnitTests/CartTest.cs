a33dd57daed7b64b73c76e12e842e9fb09cd3ae3
Svitlana Dolinovska <sdolinovska@gmail.com>
2020-01-19 23:18:11 +0200

    Creating a Custom Model Binder

--- SportsStore.UnitTests/CartTest.cs
+++ SportsStore.UnitTests/CartTest.cs
@@ -1,6 +1,11 @@
 ﻿using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SportsStore.Domain.Entities;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.WebUI.Controllers;
+using System.Web.Mvc;
+using SportsStore.WebUI.Models;
 
 namespace SportsStore.UnitTests
 {
@@ -62,7 +67,7 @@ namespace SportsStore.UnitTests
             // Act
             target.RemoveLine(p2);
             // Assert
-            Assert.AreEqual(target.Lines.Where(c => c.Product == p2).Count(), 0);
+            Assert.AreEqual(target.Lines.Count(c => c.Product == p2), 0);
             Assert.AreEqual(target.Lines.Count(), 2);
 
         }
@@ -101,6 +106,55 @@ namespace SportsStore.UnitTests
             Assert.AreEqual(target.Lines.Count(), 0);
         }
 
-
+        [TestMethod]
+        public void Can_Add_To_Cart()
+        {
+            // Arrange - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new[] {
+                new Product {ProductID = 1, Name = "P1", Category = "Apples"},
+            }.AsQueryable());
+            // Arrange - create a Cart
+            Cart cart = new Cart();
+            // Arrange - create the controller
+            CartController target = new CartController(mock.Object);
+            // Act - add a product to the cart
+            target.AddToCart(cart, 1, null);
+            // Assert
+            Assert.AreEqual(cart.Lines.Count(), 1);
+            Assert.AreEqual(cart.Lines.ToArray()[0].Product.ProductID, 1);
+        }
+        [TestMethod]
+        public void Adding_Product_To_Cart_Goes_To_Cart_Screen()
+        {
+            // Arrange - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new[] {
+                new Product {ProductID = 1, Name = "P1", Category = "Apples"},
+            }.AsQueryable());
+            // Arrange - create a Cart
+            Cart cart = new Cart();
+            // Arrange - create the controller
+            CartController target = new CartController(mock.Object);
+            // Act - add a product to the cart
+            RedirectToRouteResult result = target.AddToCart(cart, 2, "myUrl");
+            // Assert
+            Assert.AreEqual(result.RouteValues["action"], "Index");
+            Assert.AreEqual(result.RouteValues["returnUrl"], "myUrl");
+        }
+        [TestMethod]
+        public void Can_View_Cart_Contents()
+        {
+            // Arrange - create a Cart
+            Cart cart = new Cart();
+            // Arrange - create the controller
+            CartController target = new CartController(null);
+            // Act - call the Index action method
+            CartIndexViewModel result
+                = (CartIndexViewModel)target.Index(cart, "myUrl").ViewData.Model;
+            // Assert
+            Assert.AreSame(result.Cart, cart);
+            Assert.AreEqual(result.ReturnUrl, "myUrl");
+        }
     }
 }
---
+++ SportsStore.WebUI/Infrastructure/Binders/CartModelBinder.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Infrastructure.Binders
+{
+    public class CartModelBinder : IModelBinder
+    {
+        private const string SessionKey = "Cart";
+
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            // get the Cart from the session
+            var cart = (Cart) controllerContext.HttpContext.Session[SessionKey];
+
+            // create the Cart if there wasn't one in the session data
+            if (cart == null)
+            {
+                cart = new Cart();
+                controllerContext.HttpContext.Session[SessionKey] = cart;
+            }
+
+            // return the cart
+            return cart;
+        }
+    }
+}
